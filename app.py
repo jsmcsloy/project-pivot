fe5bb4fd20ecf57810fa78479920d032c01b4ba2
Jsm <jmcsloy@gmail.com>
2021-06-04 11:12:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,12 +42,7 @@ try:
 
 
 
-    table = pd.pivot_table(df,index=["Colorist","Project #"], values=["Job Card Number"],aggfunc=[len],fill_value=0)
-    writer = pd.ExcelWriter('output.xlsx')
-    for manager in table.index.get_level_values(0).unique():
-        temp_df = table.xs(manager, level=0)
-        temp_df.to_excel(writer,manager)
-    writer.save()
+ 
 
 
 except:
