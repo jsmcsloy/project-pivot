8c09c188fcaf14ce05a1146622030614bc97d47e
Jsm <jmcsloy@gmail.com>
2021-06-04 20:53:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,16 @@ project_button = st.sidebar.button("Process")
 
 if project_button == True:
     df = pd.read_excel(in_file)
+    df["Job Card Number"] = df["Job Card Number"].astype("category")
+    allocation = pd.pivot_table(df, index=["Colorist","Project #"], columns = ["Job Card Type"], values="Job Card Number", aggfunc=[len], fill_value=0 ,margins=True)
+    projects =  pd.pivot_table(df, index=["Project #"], columns = ["Colorist"], values="Job Card Number", aggfunc=[len], fill_value=0 ,margins=True)
+    
+    st.subheader("Work Allocation")
+    allocation
+    st.subheader("Projects #")
+    projects
+
+
 
     # #create teh dataframe
     # try:
