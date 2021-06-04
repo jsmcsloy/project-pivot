1004ebe678372111cf519fdefb2538725d0816b9
jsmcsloy <jmcsloy@gmail.com>
2021-06-04 20:27:43 +0100

    .

--- app.py
+++ app.py
@@ -15,46 +15,46 @@ in_file = st.sidebar.file_uploader("Pick or drag in your file")
 project_button = st.sidebar.button("Process")
 
 if project_button == True:
+df = pd.read_excel(in_file)
+
+    # #create teh dataframe
+    # try:
+    #     df = pd.read_excel(in_file)
+    #     print('read ok')
 
 
-    #create teh dataframe
-    try:
-        df = pd.read_excel(in_file)
-        print('read ok')
+    # except:
+    #     print ('failed 1')
+    #     pass
 
 
-    except:
-        print ('failed 1')
-        pass
+    # try:
 
+    #     df["Job Card Number"] = df["Job Card Number"].astype("category")
 
-    try:
-
-        df["Job Card Number"] = df["Job Card Number"].astype("category")
-
-        allocation = pd.pivot_table(df, index=["Colorist","Project #"], columns = ["Job Card Type"], values="Job Card Number", aggfunc=[len], fill_value=0 ,margins=True)
-        projects =  pd.pivot_table(df, index=["Project #"], columns = ["Colorist"], values="Job Card Number", aggfunc=[len], fill_value=0 ,margins=True)
+    #     allocation = pd.pivot_table(df, index=["Colorist","Project #"], columns = ["Job Card Type"], values="Job Card Number", aggfunc=[len], fill_value=0 ,margins=True)
+    #     projects =  pd.pivot_table(df, index=["Project #"], columns = ["Colorist"], values="Job Card Number", aggfunc=[len], fill_value=0 ,margins=True)
 
 
 
 
 
-        st.subheader("Work Allocation")
-        allocation
-        st.subheader("Projects #")
-        projects
+    #     st.subheader("Work Allocation")
+    #     allocation
+    #     st.subheader("Projects #")
+    #     projects
 
 
-        # grp = df.groupby(["Project #"])
-        # grp.describe()
+    #     # grp = df.groupby(["Project #"])
+    #     # grp.describe()
 
 
 
     
 
 
-    except:
-        st.header("Not the file we are looking for, move along, move along..")
+    # except:
+    #     st.header("Not the file we are looking for, move along, move along..")
 
 
 
