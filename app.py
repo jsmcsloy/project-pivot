96a26d1d3d5d5f8712e48d850d3c153ef6e878ec
jsmcsloy <jmcsloy@gmail.com>
2021-06-04 23:27:13 +0100

    s

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 import base64
 import openpyxl
 
-#App to pretty up excel data 
+#App to pretty up excel data
 
 st.title("Report processor v1")
 
@@ -15,16 +15,16 @@ st.sidebar.header("Project Processor")
 
 in_file = st.sidebar.file_uploader("Pick or drag in your file", type="xlsx")
 
-project_button = st.sidebar.button("Process") 
+project_button = st.sidebar.button("Process")
 
 if project_button == True:
     df = pd.read_excel(in_file)
-       
+
 
     df["Job Card Number"] = df["Job Card Number"].astype("category")
     allocation = pd.pivot_table(df, index=["Colorist","Project #"], columns = ["Job Card Type"], values="Job Card Number", aggfunc=[len], fill_value=0 ,margins=True)
     projects =  pd.pivot_table(df, index=["Project #"], columns = ["Colorist"], values="Job Card Number", aggfunc=[len], fill_value=0 ,margins=True)
- 
+
     st.subheader("Projects #")
     projects
 
@@ -33,13 +33,14 @@ if project_button == True:
     for manager in table.index.get_level_values(0).unique():
         temp_df = table.xs(manager, level=0)
         temp_df.to_excel(writer,manager)
-        #writer.save()
-    with open(writer,'rb') as f:
-        b64 = base64.b64encode(f.read())
-        href = f'<a href="data:file/xls;base64,{b64}" download="new_file.xlsx">Download xslx</a>'
-        st.write(href, unsafe_allow_html=True) 
+        fs = writer.save()
 
-   
+        
+        href = f'<a href="data:file/xls;base64,{fs}" download="new_file.xlsx">Download xslx</a>'
+
+    st.write(href, unsafe_allow_html=True)
+
+
 
 
 
@@ -49,4 +50,4 @@ if project_button == True:
         #         b64 = base64.b64encode(f.read())
         #         href = f'<a href="data:file/xls;base64,{b64}" download="new_file.{extension}">Download {extension}</a>'
 
-        # st.write(href, unsafe_allow_html=True) 
+        # st.write(href, unsafe_allow_html=True)
