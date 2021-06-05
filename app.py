0ae0b66f401159835827048e09c502b16b2e7b56
jsmcsloy <jmcsloy@gmail.com>
2021-06-05 10:55:47 +0100

    ok

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.sidebar.header("Project Processor")
 in_file = st.sidebar.file_uploader("Pick or drag in your file", type="xlsx")
 project_button = st.sidebar.button("Process")
 
-# st.sidebar.header("New Colour Processor")
+ st.sidebar.header("New Colour Processor")
 # in_file2 = st.sidebar.file_uploader("Pick or drag in your file", type="xlsx")
 # new_button = st.sidebar.button("Process ")
 
