78878438f159ef46f3807c7f45eb787d138eb64c
jsmcsloy <jmcsloy@gmail.com>
2021-06-05 09:08:49 +0100

    '

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if project_button == True:
     projects
 
     table = pd.pivot_table(df,index=["Colorist","Project #"], values=["Job Card Number"],aggfunc=[len],fill_value=0)
-    writer = pd.ExcelWriter("jsmcsloy/project-pivot/main/")
+    writer = pd.ExcelWriter("jsmcsloy/project-pivot/main/output.xslsx")
     for manager in table.index.get_level_values(0).unique():
             temp_df = table.xs(manager, level=0)
             temp_df.to_excel(writer,manager, encoding='utf-8')
