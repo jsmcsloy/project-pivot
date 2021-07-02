1eac683c67be4b9b920549efaa8c682f2e281744
Jsm <jmcsloy@gmail.com>
2021-07-02 22:45:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,10 +48,10 @@ if project_button == True:
 
 def get_binary_file_downloader_html(bin_file, file_label='File'):
         with open(bin_file, 'rb') as f:
-        data = f.read()
-        bin_str = base64.b64encode(data).decode()
-        href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
-        return href
+                data = f.read()
+                bin_str = base64.b64encode(data).decode()
+                href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
+                return href
 
 st.markdown(get_binary_file_downloader_html('output.xlsx', 'Data'), unsafe_allow_html=True)
 
