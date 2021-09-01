8922cf922c6055d1a19269e7a93aa25ce40f9bf7
Jsm <jmcsloy@gmail.com>
2021-09-01 20:55:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,10 +43,10 @@ if project_button == True:
             temp_df.to_excel(writer,manager)
         writer.save()
         def get_binary_file_downloader_html(bin_file, file_label='File'):
-        with open(bin_file, 'rb') as f:
-                data = f.read()
-                bin_str = base64.b64encode(data).decode()
-                href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
+                with open(bin_file, 'rb') as f:
+                        data = f.read()
+                        bin_str = base64.b64encode(data).decode()
+                        href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
                 return href
 
 st.markdown(get_binary_file_downloader_html('output.xlsx', 'Data'), unsafe_allow_html=True)
